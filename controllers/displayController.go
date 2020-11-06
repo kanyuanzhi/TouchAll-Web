97797a049407403910790030fb88fc773d7ff593
kanyuanzhi <kan@mail.ustc.edu.cn>
2020-11-07 01:23:19 +0800

    现在请求多个设备的实时状态信息时，只需要建立一个websocket连接

--- controllers/displayController.go
+++ controllers/displayController.go
@@ -12,8 +12,9 @@ func EquipmentDisplay(c *gin.Context) {
 	equipmentID, _ := strconv.Atoi(equipmentIDStr)
 	if equipment, has := utils.FindEquipmentByID(equipmentID); has {
 		c.HTML(http.StatusOK, "display/equipment.html", gin.H{
-			"title":     "单个设备状态展示",
-			"equipment": equipment,
+			"title":         "单个设备状态展示",
+			"equipment":     equipment,
+			"equipment_ids": []int{equipmentID},
 		})
 	} else {
 		c.String(http.StatusOK, "没有id为%s的设备", equipment.EquipmentID)
@@ -22,8 +23,13 @@ func EquipmentDisplay(c *gin.Context) {
 
 func AllEquipmentsDisplay(c *gin.Context) {
 	equipments := utils.FindAllEquipmentIDs()
+	equipmentIDs := make([]int, 0)
+	for _, equipment := range equipments {
+		equipmentIDs = append(equipmentIDs, equipment.EquipmentID)
+	}
 	c.HTML(http.StatusOK, "display/allEquipments.html", gin.H{
-		"title":      "所有设备状态展示",
-		"equipments": equipments,
+		"title":         "所有设备状态展示",
+		"equipments":    equipments,
+		"equipment_ids": equipmentIDs,
 	})
 }
